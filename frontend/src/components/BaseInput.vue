bd9dd4b164842d6727599903446ae9c94c9e3be2
Hiroki Komatsu <7227hiroki@gmail.com>
2025-09-14 20:06:47 +0900

    [update]ログイン画面・登録フォーム画面の作成

---
+++ frontend/src/components/BaseInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <input
+    v-bind="$attrs"
+    :value="modelValue"
+    @input="onInput"
+    :type="type"
+    class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-300 text-gray-700"
+  />
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps<{
+  modelValue: string
+  type?: string
+}>()
+
+const emits = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+
+const onInput = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  emits('update:modelValue', target.value)
+}
+
+const type = props.type || 'text'
+</script>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<!-- src/views/HomeView.vue -->
+<template>
+  <div>
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
