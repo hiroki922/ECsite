6f612bc50823fbd6655f6e9c0c6f72b69f70187a
Hiroki Komatsu <7227hiroki@gmail.com>
2025-10-15 22:03:56 +0900

    [update]セッション生成処理

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -3,11 +3,11 @@
     <h1 class="text-xl font-bold">ECサイト</h1>
     <nav class="space-x-4 flex items-center">
       <router-link to="/">Home</router-link>
+      <router-link to="/products">Products</router-link>
+      <router-link to="/cart">Cart</router-link>
+      <router-link to="/orders">Orders</router-link>
 
       <template v-if="auth.isLoggedIn">
-        <router-link to="/products">Products</router-link>
-        <router-link to="/cart">Cart</router-link>
-        <router-link to="/orders">Orders</router-link>
         <span class="text-gray-300 text-sm">ようこそ、{{ auth.userName }} さん</span>
         <button @click="logout" class="bg-red-500 px-3 py-1 rounded hover:bg-red-600">
           Logout
@@ -32,7 +32,7 @@ const auth = useAuthStore()
 
 async function logout(): Promise<void> {
   try {
-    await axios.post('/logout', {}, { withCredentials: true })
+    await axios.post('http://localhost:8080/api/logout', {}, { withCredentials: true })
   } finally {
     auth.clearUser()
     router.push('/login')
